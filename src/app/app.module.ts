ee65e1c8c391dd911bc95d4f57d09a944f768393
Phap <centralparkloop@yahoo.com>
2020-02-07 23:08:18 -0500

    add some components and some routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AdminComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([{
+      path: '', component: HomeComponent,
+    }, {
+      path: 'login', component: LoginComponent
+    }, {
+      path: 'admin', component: AdminComponent
+    }])
   ],
   providers: [],
   bootstrap: [AppComponent]
