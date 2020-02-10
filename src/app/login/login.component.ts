7f900b51123fe8ace2395c6f20b5100856a3135f
Phap <centralparkloop@yahoo.com>
2020-02-09 20:47:01 -0500

    created more stuff

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthService } from '../auth.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private Auth: AuthService) { }
 
   ngOnInit() {
   }
 
+  loginUser(event) {
+    event.preventDefault();
+    const username = event.target.querySelector('username');
+    const password = event.target.querySelector('password');
+    console.log('username', username, 'password', password);
+  }
 }
